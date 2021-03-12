37a5bb73b1492627fbe2205fdf57b6e2f1002d26
bestekov <4909619+bestekov@users.noreply.github.com>
2021-03-12 12:31:51 -0500

    Test fork

--- Frontend/TeelSys.Web/WebContent.cs
+++ Frontend/TeelSys.Web/WebContent.cs
@@ -16,7 +16,6 @@ namespace TeelSys.Web
                 var webRequest = WebRequest.Create(url);
 
                 using (var response = webRequest.GetResponse())
-                using (var content = response.GetResponseStream())
                 using (var reader = new StreamReader(content))
                 {
                     strContent = reader.ReadToEnd();
